aef900a3f9f119cf195de3ac62b2d41ddbaf22e6
Ítalo <italo.ts4895@gmail.com>
2023-04-05 15:45:16 -0300

    Autoformatted files and removed unnecessary using directives

--- Program.cs
+++ Program.cs
@@ -1,196 +1,198 @@
 ﻿using System;
-using static AutoBattle.Character;
-using static AutoBattle.Grid;
 using System.Collections.Generic;
 using System.Linq;
 using static AutoBattle.Types;
 
 namespace AutoBattle
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Grid grid = new Grid(5, 5);
-            CharacterClass playerCharacterClass;
-            GridBox PlayerCurrentLocation;
-            GridBox EnemyCurrentLocation;
-            Character PlayerCharacter;
-            Character EnemyCharacter;
-            List<Character> AllPlayers = new List<Character>();
-            int currentTurn = 0;
-            int numberOfPossibleTiles = grid.grids.Count;
-            Setup(); 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Grid grid = new Grid(5, 5);
+			CharacterClass playerCharacterClass;
+			GridBox PlayerCurrentLocation;
+			GridBox EnemyCurrentLocation;
+			Character PlayerCharacter;
+			Character EnemyCharacter;
+			List<Character> AllPlayers = new List<Character>();
+			int currentTurn = 0;
+			int numberOfPossibleTiles = grid.grids.Count;
+			Setup();
 
 
-            void Setup()
-            {
+			void Setup()
+			{
 
-                GetPlayerChoice();
-            }
+				GetPlayerChoice();
+			}
 
-            void GetPlayerChoice()
-            {
-                //asks for the player to choose between for possible classes via console.
-                Console.WriteLine("Choose Between One of this Classes:\n");
-                Console.WriteLine("[1] Paladin, [2] Warrior, [3] Cleric, [4] Archer");
-                //store the player choice in a variable
-                string choice = Console.ReadLine();
+			void GetPlayerChoice()
+			{
+				//asks for the player to choose between for possible classes via console.
+				Console.WriteLine("Choose Between One of this Classes:\n");
+				Console.WriteLine("[1] Paladin, [2] Warrior, [3] Cleric, [4] Archer");
+				//store the player choice in a variable
+				string choice = Console.ReadLine();
 
-                switch (choice)
-                {
-                    case "1":
-                        CreatePlayerCharacter(Int32.Parse(choice));
-                        break;
-                    case "2":
-                        CreatePlayerCharacter(Int32.Parse(choice));
-                        break;
-                    case "3":
-                        CreatePlayerCharacter(Int32.Parse(choice));
-                        break;
-                    case "4":
-                        CreatePlayerCharacter(Int32.Parse(choice));
-                        break;
-                    default:
-                        GetPlayerChoice();
-                        break;
-                }
-            }
+				switch (choice)
+				{
+					case "1":
+						CreatePlayerCharacter(Int32.Parse(choice));
+						break;
+					case "2":
+						CreatePlayerCharacter(Int32.Parse(choice));
+						break;
+					case "3":
+						CreatePlayerCharacter(Int32.Parse(choice));
+						break;
+					case "4":
+						CreatePlayerCharacter(Int32.Parse(choice));
+						break;
+					default:
+						GetPlayerChoice();
+						break;
+				}
+			}
 
-            void CreatePlayerCharacter(int classIndex)
-            {
-               
-                CharacterClass characterClass = (CharacterClass)classIndex;
-                Console.WriteLine($"Player Class Choice: {characterClass}");
-                PlayerCharacter = new Character(characterClass);
-                PlayerCharacter.Health = 100;
-                PlayerCharacter.BaseDamage = 20;
-                PlayerCharacter.PlayerIndex = 0;
-                
-                CreateEnemyCharacter();
+			void CreatePlayerCharacter(int classIndex)
+			{
 
-            }
+				CharacterClass characterClass = (CharacterClass)classIndex;
+				Console.WriteLine($"Player Class Choice: {characterClass}");
+				PlayerCharacter = new Character(characterClass);
+				PlayerCharacter.Health = 100;
+				PlayerCharacter.BaseDamage = 20;
+				PlayerCharacter.PlayerIndex = 0;
 
-            void CreateEnemyCharacter()
-            {
-                //randomly choose the enemy class and set up vital variables
-                var rand = new Random();
-                int randomInteger = rand.Next(1, 4);
-                CharacterClass enemyClass = (CharacterClass)randomInteger;
-                Console.WriteLine($"Enemy Class Choice: {enemyClass}");
-                EnemyCharacter = new Character(enemyClass);
-                EnemyCharacter.Health = 100;
-                PlayerCharacter.BaseDamage = 20;
-                PlayerCharacter.PlayerIndex = 1;
-                StartGame();
+				CreateEnemyCharacter();
 
-            }
+			}
 
-            void StartGame()
-            {
-                //populates the character variables and targets
-                EnemyCharacter.Target = PlayerCharacter;
-                PlayerCharacter.Target = EnemyCharacter;
-                AllPlayers.Add(PlayerCharacter);
-                AllPlayers.Add(EnemyCharacter);
-                AlocatePlayers();
-                StartTurn();
+			void CreateEnemyCharacter()
+			{
+				//randomly choose the enemy class and set up vital variables
+				var rand = new Random();
+				int randomInteger = rand.Next(1, 4);
+				CharacterClass enemyClass = (CharacterClass)randomInteger;
+				Console.WriteLine($"Enemy Class Choice: {enemyClass}");
+				EnemyCharacter = new Character(enemyClass);
+				EnemyCharacter.Health = 100;
+				PlayerCharacter.BaseDamage = 20;
+				PlayerCharacter.PlayerIndex = 1;
+				StartGame();
 
-            }
+			}
 
-            void StartTurn(){
+			void StartGame()
+			{
+				//populates the character variables and targets
+				EnemyCharacter.Target = PlayerCharacter;
+				PlayerCharacter.Target = EnemyCharacter;
+				AllPlayers.Add(PlayerCharacter);
+				AllPlayers.Add(EnemyCharacter);
+				AlocatePlayers();
+				StartTurn();
 
-                if (currentTurn == 0)
-                {
-                    //AllPlayers.Sort();  
-                }
+			}
 
-                foreach(Character character in AllPlayers)
-                {
-                    character.StartTurn(grid);
-                }
+			void StartTurn()
+			{
 
-                currentTurn++;
-                HandleTurn();
-            }
+				if (currentTurn == 0)
+				{
+					//AllPlayers.Sort();  
+				}
 
-            void HandleTurn()
-            {
-                if(PlayerCharacter.Health == 0)
-                {
-                    return;
-                } else if (EnemyCharacter.Health == 0)
-                {
-                    Console.Write(Environment.NewLine + Environment.NewLine);
+				foreach (Character character in AllPlayers)
+				{
+					character.StartTurn(grid);
+				}
 
-                    // endgame?
+				currentTurn++;
+				HandleTurn();
+			}
 
-                    Console.Write(Environment.NewLine + Environment.NewLine);
+			void HandleTurn()
+			{
+				if (PlayerCharacter.Health == 0)
+				{
+					return;
+				}
+				else if (EnemyCharacter.Health == 0)
+				{
+					Console.Write(Environment.NewLine + Environment.NewLine);
 
-                    return;
-                } else
-                {
-                    Console.Write(Environment.NewLine + Environment.NewLine);
-                    Console.WriteLine("Click on any key to start the next turn...\n");
-                    Console.Write(Environment.NewLine + Environment.NewLine);
+					// endgame?
 
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    StartTurn();
-                }
-            }
+					Console.Write(Environment.NewLine + Environment.NewLine);
 
-            int GetRandomInt(int min, int max)
-            {
-                var rand = new Random();
-                int index = rand.Next(min, max);
-                return index;
-            }
+					return;
+				}
+				else
+				{
+					Console.Write(Environment.NewLine + Environment.NewLine);
+					Console.WriteLine("Click on any key to start the next turn...\n");
+					Console.Write(Environment.NewLine + Environment.NewLine);
 
-            void AlocatePlayers()
-            {
-                AlocatePlayerCharacter();
+					ConsoleKeyInfo key = Console.ReadKey();
+					StartTurn();
+				}
+			}
 
-            }
+			int GetRandomInt(int min, int max)
+			{
+				var rand = new Random();
+				int index = rand.Next(min, max);
+				return index;
+			}
 
-            void AlocatePlayerCharacter()
-            {
-                int random = 0;
-                GridBox RandomLocation = (grid.grids.ElementAt(random));
-                Console.Write($"{random}\n");
-                if (!RandomLocation.ocupied)
-                {
-                    GridBox PlayerCurrentLocation = RandomLocation;
-                    RandomLocation.ocupied = true;
-                    grid.grids[random] = RandomLocation;
-                    PlayerCharacter.currentBox = grid.grids[random];
-                    AlocateEnemyCharacter();
-                } else
-                {
-                    AlocatePlayerCharacter();
-                }
-            }
+			void AlocatePlayers()
+			{
+				AlocatePlayerCharacter();
 
-            void AlocateEnemyCharacter()
-            {
-                int random = 24;
-                GridBox RandomLocation = (grid.grids.ElementAt(random));
-                Console.Write($"{random}\n");
-                if (!RandomLocation.ocupied)
-                {
-                    EnemyCurrentLocation = RandomLocation;
-                    RandomLocation.ocupied = true;
-                    grid.grids[random] = RandomLocation;
-                    EnemyCharacter.currentBox = grid.grids[random];
-                    grid.drawBattlefield(5 , 5);
-                }
-                else
-                {
-                    AlocateEnemyCharacter();
-                }
+			}
 
-                
-            }
+			void AlocatePlayerCharacter()
+			{
+				int random = 0;
+				GridBox RandomLocation = (grid.grids.ElementAt(random));
+				Console.Write($"{random}\n");
+				if (!RandomLocation.ocupied)
+				{
+					GridBox PlayerCurrentLocation = RandomLocation;
+					RandomLocation.ocupied = true;
+					grid.grids[random] = RandomLocation;
+					PlayerCharacter.currentBox = grid.grids[random];
+					AlocateEnemyCharacter();
+				}
+				else
+				{
+					AlocatePlayerCharacter();
+				}
+			}
 
-        }
-    }
+			void AlocateEnemyCharacter()
+			{
+				int random = 24;
+				GridBox RandomLocation = (grid.grids.ElementAt(random));
+				Console.Write($"{random}\n");
+				if (!RandomLocation.ocupied)
+				{
+					EnemyCurrentLocation = RandomLocation;
+					RandomLocation.ocupied = true;
+					grid.grids[random] = RandomLocation;
+					EnemyCharacter.currentBox = grid.grids[random];
+					grid.drawBattlefield(5, 5);
+				}
+				else
+				{
+					AlocateEnemyCharacter();
+				}
+
+
+			}
+
+		}
+	}
 }
--- Types.cs
+++ Types.cs
@@ -1,52 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AutoBattle
+﻿namespace AutoBattle
 {
-    public class Types
-    {
+	public class Types
+	{
 
-        public struct CharacterClassSpecific
-        {
-            CharacterClass CharacterClass;
-            float hpModifier;
-            float ClassDamage;
-            CharacterSkills[] skills;
+		public struct CharacterClassSpecific
+		{
+			CharacterClass CharacterClass;
+			float hpModifier;
+			float ClassDamage;
+			CharacterSkills[] skills;
 
-        }
+		}
 
-        public struct GridBox
-        {
-            public int xIndex;
-            public int yIndex;
-            public bool ocupied;
-            public int Index;
+		public struct GridBox
+		{
+			public int xIndex;
+			public int yIndex;
+			public bool ocupied;
+			public int Index;
 
-            public GridBox(int x, int y, bool ocupied, int index)
-            {
-                xIndex = x;
-                yIndex = y;
-                this.ocupied = ocupied;
-                this.Index = index;
-            }
+			public GridBox(int x, int y, bool ocupied, int index)
+			{
+				xIndex = x;
+				yIndex = y;
+				this.ocupied = ocupied;
+				this.Index = index;
+			}
 
-        }
+		}
 
-        public struct CharacterSkills
-        {
-            string Name;
-            float damage;
-            float damageMultiplier;
-        }
+		public struct CharacterSkills
+		{
+			string Name;
+			float damage;
+			float damageMultiplier;
+		}
 
-        public enum CharacterClass : uint
-        {
-            Paladin = 1,
-            Warrior = 2,
-            Cleric = 3,
-            Archer = 4
-        }
+		public enum CharacterClass : uint
+		{
+			Paladin = 1,
+			Warrior = 2,
+			Cleric = 3,
+			Archer = 4
+		}
 
-    }
+	}
 }
